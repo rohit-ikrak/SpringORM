7fa57cb536e5c3c44aba52b407e10900fe36cb19
ROHIT KARKI <86831121+rohit-ikrak@users.noreply.github.com>
2024-01-09 22:28:28 +0530

    Update StudentDaoImpl.java

--- src/main/java/com/spring/orm/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/orm/dao/StudentDaoImpl.java
@@ -1,57 +1,64 @@
 package com.spring.orm.dao;
-
 import java.util.List;
-
 import javax.transaction.Transactional;
-
 import org.springframework.orm.hibernate5.HibernateTemplate;
-
 import com.spring.orm.entities.Student;
 
+// Data Access Object (DAO) for interacting with Student entities in the database
 public class StudentDaoImpl {
-	
-	private HibernateTemplate hibernateTemplate; 
-	
-	//Insert data into db
-	@Transactional
-	public int insert(Student student) {
-		Integer i = (Integer) this.hibernateTemplate.save(student);
-		return i;
-	}
-	
-	//get data from db
-	public Student getStudent(int studentId) {
-		Student student = this.hibernateTemplate.get(Student.class, studentId);
-		return student;
-	}
-	
-	//get all rows
-	public List<Student> getAllStudents(){
-		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
-		return students;
-	}
-	
-	//delete a row 
-	@Transactional
-	public void deleteStudent(int studentId) {
-		Student student = this.hibernateTemplate.get(Student.class, studentId);
-		this.hibernateTemplate.delete(student);
-	}
-	
-	//update data in db
-	@Transactional
-	public void update(Student student) {
-		this.hibernateTemplate.update(student);
-		
-	}
-	
-		
-	public HibernateTemplate getHibernateTemplate() {
-		return hibernateTemplate;
-	}
 
-	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-		this.hibernateTemplate = hibernateTemplate;
-	}
+    // HibernateTemplate is used to interact with Hibernate ORM
+    private HibernateTemplate hibernateTemplate;
 
+    // Transactional annotation ensures that this method participates in a transaction
+    @Transactional
+    // Insert a new Student entity into the database
+    public int insert(Student student) {
+        // Save the student entity and get the generated identifier (ID)
+        Integer generatedId = (Integer) this.hibernateTemplate.save(student);
+        return generatedId;
+    }
+
+    // Retrieve a Student entity from the database based on studentId
+    public Student getStudent(int studentId) {
+        // Use HibernateTemplate's get method to retrieve the Student entity by ID
+        Student student = this.hibernateTemplate.get(Student.class, studentId);
+        return student;
+    }
+
+    // Retrieve all Student entities from the database
+    public List<Student> getAllStudents() {
+        // Use HibernateTemplate's loadAll method to get a list of all Student entities
+        List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+        return students;
+    }
+
+    // Transactional annotation ensures that this method participates in a transaction
+    // Delete a Student entity from the database based on studentId
+    @Transactional
+    public void deleteStudent(int studentId) {
+        // Get the Student entity by ID
+        Student student = this.hibernateTemplate.get(Student.class, studentId);
+        // Delete the Student entity
+        this.hibernateTemplate.delete(student);
+    }
+
+    // Transactional annotation ensures that this method participates in a transaction
+    // Update an existing Student entity in the database
+    @Transactional
+    public void update(Student student) {
+        // Use HibernateTemplate's update method to update the Student entity
+        this.hibernateTemplate.update(student);
+    }
+
+    // Getter method for HibernateTemplate
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    // Setter method for HibernateTemplate
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
+    }
 }
+
